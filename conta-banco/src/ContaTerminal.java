0d2cb958d08032e501782fab2eed1f5d1d2c74eb
beatrizgdc <beatrizgdcevaio@gmail.com>
2024-05-09 21:41:41 -0300

    update

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-
         Scanner scanner = new Scanner(System.in);
 
         int numeroUsuario = 0;
